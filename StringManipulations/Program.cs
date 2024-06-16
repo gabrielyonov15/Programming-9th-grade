f5db265f05e8c0cdc5dd35b306005358d202cb5f
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-16 15:00:54 +0300

    Add files via upload

---
+++ StringManipulations/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace StringManipulations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hi, Gabriel in C#";
+            Console.WriteLine("Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi," + name);
+            Console.WriteLine("Last name:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("Hi,{0} {1}", name, lastName);
+            Console.WriteLine("Surname");
+            string surname = Console.ReadLine();
+            Console.WriteLine($"Hi, {name} {lastName} {surname}!");
+            int result = string.Compare(name, lastName, true);
+            Console.WriteLine(result);
+            string str1 = string.Concat(name, surname);
+            Console.WriteLine(str1);
+            int firstIndex = str.IndexOf(",");
+            Console.WriteLine(firstIndex);
+            int secondIndex = str.IndexOf("b");
+            Console.WriteLine(secondIndex);
+            int notFound = str.IndexOf("/");
+            Console.WriteLine(notFound);
+            string nameFirst = str.Substring(4, 7);
+            Console.WriteLine(nameFirst);
+            string nameSecond = str.Substring(0, 2);
+            Console.WriteLine(nameSecond);
+            string relpaced = str.Replace("C#", "Java");
+            Console.WriteLine(relpaced);
+        }
+    }
+}
---
+++ StudentSortingByGradeAndNumber/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace StudentSortingByGradeAndNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Въведи число:");
+            int n = int.Parse(Console.ReadLine());
+            int[] grade = new int[n];
+            int[] number = new int[n];
+            string[] name = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Кой клас си:");
+                grade[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Кой номер си:");
+                number[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Как се казваш:");
+                name[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < grade.Length - 1; i++)
+            {
+                for (int j = 0; j < grade.Length - 1; j++)
+                {
+                    if (grade[j] < grade[j + 1])
+                    {
+                        int swapVar = grade[j];
+                        grade[j] = grade[j + 1];
+                        grade[j + 1] = swapVar;
+                    }
+                }
+            }
+            for (int i = 0; i < grade.Length; i++)
+            {
+                Console.WriteLine(grade[i] + " ");
+            }
+            for (int i = 0; i < number.Length - 1; i++)
+            {
+                for (int j = 0; j < number.Length - 1 - i; j++)
+                {
+                    if (number[j] > number[j + 1])
+                    {
+                        int swapVar1 = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = swapVar1;
+                    }
+                }
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine(number[i] + " ");
+            }
+            for (int i = 0; i < number.Length - 1; i++)
+            {
+                for (int j = 0; j < number.Length - 1 - i; j++)
+                {
+
+                    if (number[j] > number[j + 1])
+                    {
+                        int swapVar1 = number[j];
+                        number[j] = number[j + 1];
+                        number[j + 1] = swapVar1;
+                    }
+                    if (number[j] == number[j + 1])
+                    {
+                        if (grade[j] > grade[j + 1])
+                        {
+
+                            int swapVar2 = grade[j];
+                            grade[j] = grade[j + 1];
+                            grade[j + 1] = swapVar2;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < grade.Length; i++)
+            {
+                Console.WriteLine(grade[i] + " ");
+            }
+        }
+    }
+}
