47626ef1234596138f3a2ccc49faffcfbdb9b934
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-16 15:58:48 +0300

    Add files via upload

---
+++ ConcatenationOfTwoStringsWithSpace/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConcatenationOfTwoStringsWithSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string str1 = Console.ReadLine();
+            Console.WriteLine(String.Concat(str, " ", str1));
+        }
+    }
+}
