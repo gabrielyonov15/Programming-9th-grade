c89bbc2d919d2d669d1cde6ef56c45fab109f61e
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-16 15:01:54 +0300

    Add files via upload

---
+++ SubjectsAndHoursManagement/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace SubjectsAndHoursManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] subject = new string[3];
+            double[] hour = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Enter subject:");
+                subject[i] = Console.ReadLine();
+                Console.WriteLine("Enter time:");
+                hour[i] = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Output 1");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(subject[i]);
+                Console.WriteLine(hour[i]);
+            }
+            Console.WriteLine("Output 2");
+            for (int i = 0; i < 3; i++)
+            {
+                if (hour[i] >= 4)
+                {
+                    Console.WriteLine(hour[i]);
+                }
+            }
+            Console.WriteLine("Output 3");
+            Array.Sort(hour);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(hour[i]);
+            }
+            Console.WriteLine("Output 4");
+            Array.Sort(subject);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(subject[i]);
+            }
+            Console.WriteLine("Output 5");
+            Array.Reverse(subject);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(subject[i]);
+            }
+            Console.WriteLine("Output 6");
+            string newSubject = "Maths";
+            for (int i = 0; i < 3; i++)
+            {
+                if (subject[i] == newSubject)
+                {
+                    Console.WriteLine(subject[i]);
+                    Console.WriteLine(hour[i]);
+                }
+            }
+            Console.WriteLine("Output 7");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(newSubject[1]);
+                Console.WriteLine(hour[1]);
+            }
+        }
+    }
+}
---
+++ SummationOfASeries/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace SummationOfASeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Каква оценка искаш?");
+            int x = int.Parse(Console.ReadLine());
+            int P = 0;
+            int a = 6;
+            do
+            {
+                P = a * x + P;
+                a = a + 1;
+            }
+            while (a <= 6);
+            {
+                Console.WriteLine("P=" + P);
+            }
+        }
+    }
+}
