a1261d31226d7104cae0ddddda53f72b9f332183
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2024-06-16 14:55:46 +0300

    Add files via upload

---
+++ DisplayingNumbersEndingWith5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DisplayingNumbersEndingWith5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 1; i <= 1000; i++)
+            {
+                if (i % 10 == 5)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
---
+++ FactorialCalculation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FactorialCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int P = 1;
+            for (var a = 7; a >= 1; a--)
+            {
+                P = a * P;
+            }
+            Console.WriteLine(P);
+        }
+    }
+}
